29487b68df314b339ace8a3e87599f5fe798f9fd
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-14 15:55:00 -0500

    implemented a hook that counts the number of rerenders

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 const COLORS = ["pink", "green", "blue", "yellow", "purple"]
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0])
+  const [renders, setRenders] = useState(-1)
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color)
   }
 
+  const countClicks = () => {
+    setRenders( renders + 1)
+  }
+
+  useEffect(() => {
+    countClicks()
+  }, [backgroundColor])
+
   return (
     <>
+      <div>Number of Rerenders: {renders}</div>
+
       <div
         className='App'
         style={{
